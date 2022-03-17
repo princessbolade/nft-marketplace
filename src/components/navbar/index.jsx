77a8dfa4e6c0e351dbc024e9ac7b73f28982b1b8
@mobolade <@mobolade.akinlua@outlook.com>
2022-03-17 14:29:08 +0100

    feat:added horizontal line

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -16,20 +16,29 @@ import React from "react";
 
 function Nav() {
   return (
-    <Box mx={"auto"} maxW="1200px">
+    <Box mx={"auto"} maxW="1200px" height={"120px"}>
       <Flex alignItems={"center"} justifyContent={"space-between"}>
         <Image src={Logo} w={"150px"} h={"150px"} color="#ec7027" />
         <UnorderedList listStyleType={"none"}>
           <Flex alignItems={"center"} justifyContent={"space-between"}>
-            <ListItem>MARKETPLACE</ListItem>
-            <ListItem pl={"40px"} fontWeight={"bold"}>
+            <ListItem fontSize={"14px"}>MARKETPLACE</ListItem>
+            <ListItem fontSize={"14px"} pl={"40px"} fontWeight={"bold"}>
               EXPLORE
             </ListItem>
-            <ListItem pl={"40px"}>CREATE</ListItem>
+            <ListItem fontSize={"14px"} pl={"40px"}>
+              CREATE
+            </ListItem>
           </Flex>
         </UnorderedList>
         <Flex alignItems={"center"} justifyContent={"flex-end"}>
-          <Box as="button" bg={"#7222F0"} color="white" padding={"12px 22px"}>
+          <Box
+            as="button"
+            textTransform={"uppercase"}
+            fontSize={"14px"}
+            bg={"#7222F0"}
+            color="white"
+            padding={"12px 22px"}
+          >
             Connect Wallet
           </Box>
         </Flex>
