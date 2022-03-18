207e1902980473d501a4eada963fba201dc930d9
@mobolade <@mobolade.akinlua@outlook.com>
2022-03-18 12:03:51 +0100

    feat:sponsors

---
+++ src/components/top-contributors/index.jsx
@@ -0,0 +1,16 @@
+import { Box, Flex, Heading } from "@chakra-ui/react";
+import React from "react";
+
+function TopContributors() {
+  return (
+    <Box mx={"auto"} maxW={"1440px"} py={"50px"}>
+      <Box>
+        <Flex>
+          <Heading></Heading>
+        </Flex>
+      </Box>
+    </Box>
+  );
+}
+
+export default TopContributors;
--- src/screens/home/index.jsx
+++ src/screens/home/index.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import Explore from "../../components/explore";
+import binance from "../../assets/binance.svg";
+import eth from "../../assets/eth.svg";
+import blockchain from "../../assets/blockchain.svg";
 import Hero from "../../components/hero";
 import Market from "../../components/market-hero";
 import Nav from "../../components/navbar";
 import SubHero from "../../components/sub-hero";
+import { Box, Image, Flex } from "@chakra-ui/react";
+import TopContributors from "../../components/top-contributors";
+
+const sponsors = [{ src: binance }, { src: eth }, { src: blockchain }];
 
 function Home() {
   return (
@@ -23,6 +30,20 @@ function Home() {
       <SubHero />
       <Explore />
       <Market />
+      <Flex
+        alignItems={"center"}
+        flexDir={"row"}
+        justifyContent={"space-between"}
+        py={"50px"}
+        bg={"linear-gradient(257.38deg, #7222F0 0%, #F72DB1 100%)"}
+        mx={"auto"}
+        maxW={"1440px"}
+      >
+        {sponsors.map((items, i) => (
+          <Image key={i} src={items.src} w={"100%"} h={"50px"} />
+        ))}
+      </Flex>
+      <TopContributors />
     </>
   );
 }
