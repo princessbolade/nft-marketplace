8bc07eca10013880270ea58b51371b0fccd07f41
@mobolade <@mobolade.akinlua@outlook.com>
2022-03-18 16:35:08 +0100

    top collections

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -8,7 +8,7 @@ import { ArrowForwardIcon } from "@chakra-ui/icons";
 function Hero() {
   return (
     <>
-      <Box mx={"auto"} maxWidth={"1440px"} py={"50px"}>
+      <Box mx={"auto"} maxWidth={"1440px"} py={"100px"}>
         <Flex alignItems={"center"} justifyContent={"space-between"}>
           <Box>
             <Box>
--- src/components/top-contributors/index.jsx
+++ src/components/top-contributors/index.jsx
@@ -1,16 +1,125 @@
-import { Box, Flex, Heading } from "@chakra-ui/react";
+import { Box, Flex, Text, Image } from "@chakra-ui/react";
+import Bubbles from "../../assets/bubbles.svg";
+import girl from "../../assets/lilgirl.svg";
+import swirls from "../../assets/swirls.svg";
+import yinman from "../../assets/yinman.svg";
+import circle from "../../assets/circle.svg";
 import React from "react";
 
-function TopContributors() {
+const btn = ["Art", "Sport", "Photography", "Pattern"];
+
+const item = [
+  {
+    src: girl,
+    text: "@johnny",
+    text2: " 0.005 ETH",
+    btnText: " Place Bid",
+    btnTextTwo: " Share",
+  },
+  {
+    src: swirls,
+    text: "@johnny",
+    text2: " 0.005 ETH",
+    btnText: " Place Bid",
+    btnTextTwo: " Share",
+  },
+  {
+    src: yinman,
+    text: "@johnny",
+    text2: " 0.005 ETH",
+    btnText: " Place Bid",
+    btnTextTwo: " Share",
+  },
+  {
+    src: circle,
+    text: "@johnny",
+    text2: " 0.005 ETH",
+    btnText: " Place Bid",
+    btnTextTwo: " Share",
+  },
+];
+
+function TopSellers() {
   return (
-    <Box mx={"auto"} maxW={"1440px"} py={"50px"}>
+    <Box mx={"auto"} maxW={"1440px"} py={"100px"}>
       <Box>
-        <Flex>
-          <Heading></Heading>
+        <Text fontWeight={"bold"} fontSize={"24px"}>
+          C O L L E C T I O N
+        </Text>
+        <Image src={Bubbles} w={"150px"} pt={"15px"} />
+        <Flex alignItems={"center"} justifyContent={"flex-start"} pt={"30px"}>
+          {btn.map((i) => (
+            <Box
+              as="button"
+              padding={"10px 32px"}
+              bg={"#7222F0"}
+              color={"white"}
+              mr={"15px"}
+              fontSize={"16px"}
+              borderRadius={"10px"}
+            >
+              {i}
+            </Box>
+          ))}
         </Flex>
       </Box>
+      <Flex alignItems={"center"} justifyContent={"space-between"} pt={"50px"}>
+        {item.map((items, i) => (
+          <Box
+            padding={"20px"}
+            bg={
+              "linear-gradient(0deg, rgba(255, 255, 255, 0.9), rgba(255, 255, 255, 0.9)), #F72DB1"
+            }
+            mr={"20px"}
+            borderRadius={"20px"}
+          >
+            <Image src={items.src} />
+            <Flex
+              alignItems={"center"}
+              justifyContent="space-between"
+              mt={"10px"}
+              padding={"20px"}
+            >
+              <Text fontWeight={"bold"}>{items.text}</Text>
+              <Text fontSize={"24px"} fontWeight={"bold"}>
+                {items.text2}
+              </Text>
+            </Flex>
+            <Flex>
+              <Box
+                as="button"
+                textTransform={"uppercase"}
+                fontSize={"14px"}
+                bg={"#7222F0"}
+                color="white"
+                mx={"auto"}
+                padding={"16px 32px"}
+                mt={"25px"}
+                textAlign={"center"}
+              >
+                {items.btnText}
+              </Box>
+              <Box
+                as="button"
+                textTransform={"uppercase"}
+                fontSize={"14px"}
+                bg={"transparent"}
+                color="black"
+                border={"1px"}
+                borderColor={"#7222F0"}
+                mx={"auto"}
+                padding={"16px 32px"}
+                mt={"25px"}
+                textAlign={"center"}
+              >
+                {items.btnTextTwo}
+              </Box>
+            </Flex>
+          </Box>
+        ))}
+      </Flex>
     </Box>
   );
 }
 
-export default TopContributors;
+export default TopSellers;
